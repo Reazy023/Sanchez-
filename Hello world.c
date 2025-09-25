95094ed341af675b73e5fda1cbbfe63be697358e
Sanchez ochieng <sanchezochieng43@gmail.com>
2025-09-25 12:27:22 +0300

    Add files via upload
    1st C program

---
+++ Hello world.c
@@ -0,0 +1,25 @@
+//simple C program (Hello world)
+/*
+Name; Sanchez Ochieng 
+Reg no;CT101/G/26450/25
+Description;Hello world c
+*/
+
+#include<stdio.h>
+    
+// main function
+int main(){
+float height;
+double bankBalance;
+int phoneNo;
+
+printf("Enter your height");
+scanf("%f" ,& height);
+
+printf("Enter your bank balance");
+scanf("%lf" ,& bankBalance);
+
+printf("Enter your phone number");
+scanf("%d");
+
+}
